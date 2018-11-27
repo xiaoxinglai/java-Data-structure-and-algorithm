2d07c6993534e1836e8952cc450ea981f2bd0237
laixiaoxing <291826425@qq.com>
2018-11-27 17:32:24 +0800

    数组定义

---
+++ array/src/main/java/impl/ArrayImpl.java
@@ -0,0 +1,23 @@
+package impl;
+
+import inteface.IArray;
+
+public class ArrayImpl implements IArray {
+
+
+    public void InitArray(int dim, int... lenths) {
+
+    }
+
+    public void DestroyArray() {
+
+    }
+
+    public Object Value(int... n) {
+        return null;
+    }
+
+    public void Assign(Object elem, int... n) {
+
+    }
+}
